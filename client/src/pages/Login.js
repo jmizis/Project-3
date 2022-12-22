e283f076062f4d5a0579edaeeb5fd855dd26f2e4
katsaymeow <107221758+katsaymeow@users.noreply.github.com>
2022-12-21 21:20:42 -0500

    login js and concurrently npm package

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,1 +1,43 @@
 // login page
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';// allows mutations thru typedefs
+import { LOGIN_USER } from '../utils/mutations';// added thru mutations
+import { Auth } from '../utils/auth';
+
+const Login = (props) => {
+    const [formState, setFormState] = useState({ email: '', password: ''});
+    const [login, {error, data}] = useMutation(LOGIN_USER);
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+        try {
+            const { data } = await login({
+                variables: { ...formState },
+            });
+            Auth.login(data.login.token);
+        } catch (e) {
+            console.log(e);
+        }
+
+        setFormState({
+            email: '',
+            password: '',
+        });
+    };
+
+    return (
+        <main>
+            <div></div>
+        </main>
+    )
+}
