ddbc5bc62df3175541b0444c1e3028335a181537
katsaymeow <107221758+katsaymeow@users.noreply.github.com>
2022-12-27 21:18:22 -0500

    switched react-router-dom from 6.5.0 to 6.2.1 and worked in the App.js for the router issue. Added the dependencies for Chakra UI and will explain in more detail on the notes in google docs how it is used. The video lessons use a different tech with it and now I understand how that is working with our App

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import Navbar from '../Navbar'
+// import Navbar from '../Navbar'
 
 
 
 const Header = () => {
   return (
       <header >
-      < Navbar />
+      {/* < Navbar /> */}
+      All i want is a wok
       </header>
     
   )
