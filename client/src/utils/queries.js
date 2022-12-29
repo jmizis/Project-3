1a914de73ef154465deb71fa0832e9a17906fb04
jmizis <107073756+jmizis@users.noreply.github.com>
2022-12-29 15:17:59 -0500

    queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -3,6 +3,7 @@ import { gql } from '@apollo/client';
 // set the match to returning the type from typeDefs
 
 // What colums we want to show up
+// need add more
 export const QUERY_USER = gql`
   query user($username: String!) {
     user(username: $username) {
