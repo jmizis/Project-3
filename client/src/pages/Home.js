120d4ae086849a931a49c3ae665778a2e1458588
katsaymeow <107221758+katsaymeow@users.noreply.github.com>
2023-01-04 16:03:25 -0500

    need to commit before I get to far in to this. trying to get our roots right. Will need to move on to the tools and techn page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,30 +1,22 @@
-// our home page or landing page. will need login page. 
-// needs to direct to login page and once creds are evaluated, this can lead to inventory input or pages to view inventory class? need to lock down these details. 
-
 import React from "react";
-// import {useMatch, useResolvedPath} from "react-router-dom";
 
-// import Login from './Login';
-// import Signup from './Signup';
 
 export default function Home() {
-  // function CustomLink({ to, children, ...props }) {
-  //   const resolvedPath = useResolvedPath(to)
-  //   const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-   
-    return (
-      <div className="grid container mx-auto content-center min-h-fit min-w-fit  text-white bg-gray-600">
-        <div className="h-16 grid content-center justify-center p-20">
-           <h2 className="h-16 grid content-center justify-center text-7xl">Welcome To TrackIt</h2>
-        </div>
-         <div className="flex container mx-auto justify-center min-h-fit min-w-fit  text-white bg-gray-600">
-           <h2>
-             This is a paragraph about us and the software.
-             To make the home page look good!
-             add some images etc.
-           </h2>
-         </div>
+
+
+  return (
+    <div className="grid container mx-auto content-center min-h-fit min-w-fit  text-white bg-gray-600">
+      <div className="h-16 grid content-center justify-center p-20">
+        <h2 className="h-16 grid content-center justify-center text-7xl">
+          Welcome To TrackIt
+        </h2>
+      </div>
+      <div className="flex container mx-auto justify-center min-h-fit min-w-fit  text-white bg-gray-600">
+        <h2>
+          This is a paragraph about us and the software. To make the home page
+          look good! add some images etc.
+        </h2>
+      </div>
     </div>
-       
   );
-};
+}
